d44176c97decbf7aa1b22423f9bcd238da985c0f
wujinsheng1 <wujinsheng1@yy.com>
2021-12-01 11:15:17 +0800

    优化代码结构

--- app/src/main/java/com/salton123/easemob/Facade.kt
+++ app/src/main/java/com/salton123/easemob/Facade.kt
@@ -5,7 +5,23 @@ package com.salton123.easemob
  * Author:
  * Description:
  */
-class Facade {
+object Facade {
+
+    init {
+        System.loadLibrary("easemob-facade")
+    }
+
+    /**
+     * 创建账号
+     * @param username 账号名
+     * @param password 密码
+     *
+     */
     external fun createAccount(username: String, password: String)
-//    external fun version(): String
+
+    /**
+     * 当前版本号
+     * @return 当前版本号
+     */
+    external fun version(): String
 }
